ab027d4023d81f3c270fd5d6a1ecaad59222a19d
deepu2126 <117104969+deepu2126@users.noreply.github.com>
2023-12-14 22:51:01 +0530

    Page layout UI (#3)
    * tailwind setup
    
    * UI - Navbar UI implementation
    
    * UI - Added Heading and para

--- best-product/src/App.js
+++ best-product/src/App.js
@@ -5,6 +5,21 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <div className="m-5">
+        <h1 className="text-3xl font-bold">
+          A Clear Guide to Choosing the{" "}
+          <span className="text-pink-600">Right One</span> <br /> Without the
+          Confusion
+        </h1>
+        <p className="text-center px-48 text-sm mt-3 font-medum text-slate-700">
+          Embark on a seamless journey to find your perfect match! Our guide{" "}
+          <br />
+          empowers you to navigate through the product landscape with clarity,
+          <br />
+          ensuring confident decisions and minimal confusion.
+        </p>
+      </div>
     </div>
   );
 }

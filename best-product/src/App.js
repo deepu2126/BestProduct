0f9752722bd5d3b351ea0637f7da6ff644fb05d8
deepu2126 <deepakmdeepu26@gmail.com>
2023-12-16 21:41:21 +0530

    adding some content

--- best-product/src/App.js
+++ best-product/src/App.js
@@ -3,10 +3,10 @@ import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="p-5">
       <NavBar />
       <div className="m-5">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold text-center">
           A Clear Guide to Choosing the{" "}
           <span className="text-pink-600">Right One</span> <br /> Without the
           Confusion
@@ -18,6 +18,34 @@ function App() {
           <br />
           ensuring confident decisions and minimal confusion.
         </p>
+      </div>
+      <div>
+        How to implement it:
+        <ul className="flex gap-5">
+          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
+            Define your needs and priorities: What are you looking for in a
+            product? What features are essential, and what are nice-to-haves?
+          </li>
+          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
+            Research and gather options: Look for products that fit your needs
+            and budget.
+          </li>
+          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
+            Group the options: Categorize them based on relevant criteria.
+          </li>
+          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
+            Develop a ranking system: Determine your criteria for ranking within
+            each group (e.g., price, features, brand reputation).
+          </li>
+          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
+            Rank and compare: Compare the options within each group based on
+            your ranking system.
+          </li>
+          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
+            Choose the best fit: Select the product that best aligns with your
+            priorities within your chosen group.
+          </li>
+        </ul>
       </div>
     </div>
   );

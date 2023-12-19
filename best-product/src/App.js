3d1ddef79df1d591a7761ae660b4665091b8a501
deepu2126 <117104969+deepu2126@users.noreply.github.com>
2023-12-19 20:43:36 +0530

    Page layout UI (#7)
    * tailwind setup
    
    * UI - Navbar UI implementation
    
    * UI - Added Heading and para
    
    * adding some content
    
    * Card UI fixes and step description

--- best-product/src/App.js
+++ best-product/src/App.js
@@ -1,53 +1,30 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import InitalCard from "./components/commonComponents/IntialCard";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
-      <div className="m-5">
-        <h1 className="text-3xl font-bold">
-          A Clear Guide to Choosing the{" "}
-          <span className="text-pink-600">Right One</span> <br /> Without the
-          Confusion
-        </h1>
-        <p className="text-center px-48 text-sm mt-3 font-medum text-slate-700">
-          Embark on a seamless journey to find your perfect match! Our guide{" "}
-          <br />
-          empowers you to navigate through the product landscape with clarity
-          <br />
-          ensuring confident decisions and minimal confusion.
-        </p>
-      </div>
-      <div>
-        How to implement it:
-        <ul className="flex gap-5">
-          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
-            Define your needs and priorities: What are you looking for in a
-            product? What features are essential, and what are nice-to-haves?
-          </li>
-          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
-            Research and gather options: Look for products that fit your needs
-            and budget.
-          </li>
-          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
-            Group the options: Categorize them based on relevant criteria.
-          </li>
-          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
-            Develop a ranking system: Determine your criteria for ranking within
-            each group (e.g., price, features, brand reputation).
-          </li>
-          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
-            Rank and compare: Compare the options within each group based on
-            your ranking system.
-          </li>
-          <li className="w-[1/6] bg-gradient-to-r from-sky-400 to-blue-500 p-2">
-            Choose the best fit: Select the product that best aligns with your
-            priorities within your chosen group.
-          </li>
-        </ul>
-      </div>
+      <main className="p-2">
+        <div className="m-5">
+          <h1 className="text-3xl font-bold">
+            A Clear Guide to Choosing the{" "}
+            <span className="text-pink-600">Right One</span> <br /> Without the
+            Confusion
+          </h1>
+          <p className="text-center px-48 text-sm mt-3 font-medum text-slate-700">
+            Embark on a seamless journey to find your perfect match! Our guide{" "}
+            <br />
+            empowers you to navigate through the product landscape with clarity
+            <br />
+            ensuring confident decisions and minimal confusion.
+          </p>
+        </div>
+        <div>
+          <InitalCard />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ best-product/src/components/commonComponents/IntialCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Card({ step, content }) {
+  return (
+    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{content}</p>
+      <a
+        href="#"
+        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        {step + 1}
+        <svg
+          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </a>
+    </div>
+  );
+}
+
+export default function IntialCard() {
+  const stepToExplore = [
+    "Define your needs and priorities: What are you looking for in a product? What features are essential, and what are nice-to-haves?",
+    "Research and gather options: Look for products that fit your needs and budget.",
+    "Group the options: Categorize them based on relevant criteria.",
+    "Develop a ranking system: Determine your criteria for ranking within each group (e.g., price, features, brand reputation).",
+    "Rank and compare: Compare the options within each group based on your ranking system.",
+    "Choose the best fit: Select the product that best aligns with your priorities within your chosen group.",
+  ];
+
+  return (
+    <div className="grid grid-cols-4 gap-3">
+      {stepToExplore.map((item, index) => (
+        <Card key={index} step={index} content={item} />
+      ))}
+    </div>
+  );
+}
